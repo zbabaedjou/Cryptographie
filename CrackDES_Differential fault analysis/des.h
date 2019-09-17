7bfe2be4777b2e485efcd19cf10d73a1e7ab3bd9
zbabaedjou <babaedjouziadath@gmail.com>
2019-09-17 16:40:37 +0200

    Ajout du CrackDes DFA

---
+++ CrackDES_Differential fault analysis/des.h
@@ -0,0 +1,12 @@
+#ifndef DES_H_INCLUDED
+#define DES_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+
+	void key64to48( int key[]);
+    int Encryption();
+
+#endif // DES_H_INCLUDED
